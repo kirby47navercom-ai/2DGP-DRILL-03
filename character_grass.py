619885fd1ca2c1386a811b329bf0c56c4296844b
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-09-17 13:10:25 +0900

    캐릭터 출력 구현 완료

--- character_grass.py
+++ character_grass.py
@@ -2,17 +2,24 @@ from pico2d import *
 #하나하나할때마다 커밋을해야함
 open_canvas()
 
+boy = load_image('character.png')
+
 def move_rectangle():
     print("Moving rectangle")
     pass
 def move_circle():
     print("Moving circle")
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    #무조건 now로 만들기
+    delay(0.1)
     pass
 
 while True:
     #함수명을 이해가 가도록 적어야함
-    move_rectangle()
     move_circle()
+    move_rectangle()
+    #break
     pass
 
 close_canvas()
