846064b25d4b6f5f21d5e65b2559f62fcc10915e
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-09-17 13:06:45 +0900

    사각운동, 원운동 함수 뼈대 완성.

--- character_grass.py
+++ character_grass.py
@@ -2,7 +2,15 @@ from pico2d import *
 #하나하나할때마다 커밋을해야함
 open_canvas()
 
+def move_rectangle():
+    pass
+def move_circle():
+    pass
+
 while True:
+    #함수명을 이해가 가도록 적어야함
+    move_rectangle()
+    move_circle()
     pass
 
 close_canvas()
